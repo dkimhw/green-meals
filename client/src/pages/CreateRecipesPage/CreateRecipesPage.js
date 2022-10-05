d2cdfc0a33d0caf6c725c6b6e5918b82bdc4a2e2
David Kim <dkhw90@gmail.com>
2022-10-05 23:02:56 +0200

    Start create recipes page

--- client/src/pages/CreateRecipesPage/CreateRecipesPage.js
+++ client/src/pages/CreateRecipesPage/CreateRecipesPage.js
@@ -1,12 +1,26 @@
 
 import React from 'react';
-
+import { Box, TextField, Typography } from '@mui/material';
+import classes from './CreateRecipesPage.module.css';
 
 const CreateRecipes = () => {
   return (
-    <div>
-      <h1>Create Recipe</h1>
-    </div>
+    <React.Fragment>
+      <Typography variant="h3" className={classes['create-recipes-title']} align="center">Create a Recipe</Typography>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1 },
+          'display': 'flex',
+          'justify-content': 'center'
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Recipe Name" variant="outlined"/>
+        <TextField id="outlined-basic" label="Description" variant="outlined"/>
+      </Box>
+    </React.Fragment>
   );
 }
 
