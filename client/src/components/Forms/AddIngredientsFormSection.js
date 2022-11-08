4f646bc8f6d8e931316d4609dc0d1346d3f19b30
David Kim <dkhw90@gmail.com>
2022-11-08 16:25:02 +0100

    Simplify adding ingredients; add remove button

--- client/src/components/Forms/AddIngredientsFormSection.js
+++ client/src/components/Forms/AddIngredientsFormSection.js
@@ -1,119 +1,66 @@
 
 import React from 'react';
-import { TextField, Button, MenuItem } from '@mui/material';
+import { TextField, Button, IconButton } from '@mui/material';
 import classes from './AddIngredientsFormSection.module.css';
-
-
-const qty_types = [
-  {
-    value: 'Cup',
-    label: 'Cup'
-  },
-  {
-    value: 'Tablespoon',
-    label: 'Tbsp'
-  },
-  {
-    value: 'Teaspoon',
-    label: 'Tsp'
-  },
-  {
-    value: 'Ounces',
-    label: 'Oz'
-  },
-];
+import CloseIcon from '@mui/icons-material/Close';
 
 // https://stackoverflow.com/questions/66469913/how-to-add-input-field-dynamically-when-user-click-on-button-in-react-js
 const AddIngredientsFormSection = () => {
   const ingredientsInputs = [
-    { id: 0, recipe_name: '', qty: 0, qty_type: '', placeholder: 'e.g. Flour' },
-    { id: 1, recipe_name: '', qty: 0, qty_type: '', placeholder: 'e.g. Sugar' },
-    { id: 2, recipe_name: '', qty: 0, qty_type: '', placeholder: 'e.g. Olive oil' },
+    { id: 0, ingredient_name: '', placeholder: 'e.g. Flour' },
+    { id: 1, ingredient_name: '', placeholder: 'e.g. Sugar' },
+    { id: 2, ingredient_name: '', placeholder: 'e.g. Olive oil' },
   ]
   const [ingredients, setIngredients] = React.useState(ingredientsInputs);
-  const [quantityType, setQuantityType] = React.useState('Cup');
 
   const addNewInput = () => {
-    setIngredients([...ingredients, { id: `ingredient-name${ingredients.length}`, name: `ingredientName${ingredients.length}`, placeholder: 'Add a new ingredient' }]);
-    console.log(ingredients);
+    let maxId = Math.max(...ingredients.map(ingredient => ingredient.id));
+    maxId < 0 ? maxId = 0 : maxId = maxId + 1;
+    setIngredients([...ingredients, { id: maxId, ingredient_name: '', placeholder: 'Add a new ingredient' }]);
   };
 
-  // const removeInput = (index) => {
-  //   const values = [...ingredients];
-  //   values.splice(index, 1);
-  //   setIngredients(values);
-  // };
+  const removeIngredient = (id) => {
+    console.log(id);
+    const values = [...ingredients];
+    values.splice(values.findIndex(value => value.id === id), 1);
+    // const filteredValues = values.filter(ingredient => ingredient.id !== id);
+    // console.log(filteredValues);
+    console.log('values', values);
 
-  const handleRecipeNameChange = (event) => {
+    setIngredients(values);
+  };
+
+  const handleIngredientNameChange = (event) => {
     event.preventDefault();
     let splitIdLen = event.target.id.split('-').length
-    const index = event.target.id.split('-')[splitIdLen - 1];
+    const id = event.target.id.split('-')[splitIdLen - 1];
+    let findIdx = ingredients.findIndex(ingredient => ingredient.id === parseInt(id));
     const values = [...ingredients];
-    values[index].recipe_name = event.target.value;
+    values[findIdx].ingredient_name = event.target.value;
     setIngredients(values);
     console.log(ingredients);
   };
 
-  const handleQtyChange = (event) => {
-    event.preventDefault();
-    let splitIdLen = event.target.id.split('-').length
-    const index = event.target.id.split('-')[splitIdLen - 1];
-    const values = [...ingredients];
-    values[index].qty = event.target.value;
-    setIngredients(values);
-    console.log(ingredients);
-  };
-
-  const handleQtyTypeChange = (event) => {
-    event.preventDefault();
-    console.log(event);
-    let splitIdLen = event.target.name.split('-').length
-    const index = event.target.name.split('-')[splitIdLen - 1];
-    const values = [...ingredients];
-    values[index].qty_type = event.target.value;
-    setIngredients(values);
-    console.log(ingredients);
-  };
 
 
   return (
     <React.Fragment>
-      {ingredients.map((input, index) => {
+      {ingredients.map((input) => {
         return (
-          <div className={classes['ingredients-form-group']} key={index}>
+          <div className={classes['ingredients-form-group']} key={input.id}>
             <TextField
               id={`recipe-name-${input.id}`}
               name={`recipe-name-${input.id}`}
               placeholder={input.placeholder}
               defaultValue=""
-              onChange={handleRecipeNameChange}
+              onChange={handleIngredientNameChange}
               variant="outlined"
+              values={input.ingredient_name}
               label="Ingredient Name"
             />
-            <TextField
-              id={`recipe-qty-${input.id}`}
-              name={`recipe-qty-${input.id}`}
-              placeholder={`e.g. ${Math.ceil(Math.random() * 10)}`}
-              defaultValue=""
-              onChange={handleQtyChange}
-              variant="outlined"
-              label="Quantity"
-            />
-            <TextField
-              select
-              id = {`recipe-qty-type-${input.id}`}
-              name = {`recipe-qty-type-${input.id}`}
-              label="Choose Qty Type"
-              value='Cup'
-              onChange={handleQtyTypeChange}
-            >
-              {qty_types.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
-
+            <IconButton id={`remove-recipe-name-${input.id}`}  color="primary" onClick={() => removeIngredient(input.id)} aria-label="remove ingredient" component="label">
+              <CloseIcon sx={{fontSize: '1.75rem'}}/>
+            </IconButton>
           </div>
         )
       })}
