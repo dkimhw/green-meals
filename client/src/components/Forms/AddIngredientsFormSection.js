3233870020cbd39f0c4a71e36881e23c380001ca
David Kim <dkhw90@gmail.com>
2022-11-09 15:12:57 +0100

    Link add recipe form with ingredients add form section component

--- client/src/components/Forms/AddIngredientsFormSection.js
+++ client/src/components/Forms/AddIngredientsFormSection.js
@@ -4,48 +4,10 @@ import { TextField, Button, IconButton } from '@mui/material';
 import classes from './AddIngredientsFormSection.module.css';
 import CloseIcon from '@mui/icons-material/Close';
 
-// https://stackoverflow.com/questions/66469913/how-to-add-input-field-dynamically-when-user-click-on-button-in-react-js
-const AddIngredientsFormSection = () => {
-  const ingredientsInputs = [
-    { id: 0, ingredient_name: '', placeholder: 'e.g. Flour' },
-    { id: 1, ingredient_name: '', placeholder: 'e.g. Sugar' },
-    { id: 2, ingredient_name: '', placeholder: 'e.g. Olive oil' },
-  ]
-  const [ingredients, setIngredients] = React.useState(ingredientsInputs);
-
-  const addNewInput = () => {
-    let maxId = Math.max(...ingredients.map(ingredient => ingredient.id));
-    maxId < 0 ? maxId = 0 : maxId = maxId + 1;
-    setIngredients([...ingredients, { id: maxId, ingredient_name: '', placeholder: 'Add a new ingredient' }]);
-  };
-
-  const removeIngredient = (id) => {
-    console.log(id);
-    const values = [...ingredients];
-    values.splice(values.findIndex(value => value.id === id), 1);
-    // const filteredValues = values.filter(ingredient => ingredient.id !== id);
-    // console.log(filteredValues);
-    console.log('values', values);
-
-    setIngredients(values);
-  };
-
-  const handleIngredientNameChange = (event) => {
-    event.preventDefault();
-    let splitIdLen = event.target.id.split('-').length
-    const id = event.target.id.split('-')[splitIdLen - 1];
-    let findIdx = ingredients.findIndex(ingredient => ingredient.id === parseInt(id));
-    const values = [...ingredients];
-    values[findIdx].ingredient_name = event.target.value;
-    setIngredients(values);
-    console.log(ingredients);
-  };
-
-
-
+const AddIngredientsFormSection = (props) => {
   return (
     <React.Fragment>
-      {ingredients.map((input) => {
+      {props.ingredients.map((input) => {
         return (
           <div className={classes['ingredients-form-group']} key={input.id}>
             <TextField
@@ -53,19 +15,19 @@ const AddIngredientsFormSection = () => {
               name={`recipe-name-${input.id}`}
               placeholder={input.placeholder}
               defaultValue=""
-              onChange={handleIngredientNameChange}
+              onChange={props.handleIngredientNameChange}
               variant="outlined"
               values={input.ingredient_name}
               label="Ingredient Name"
             />
-            <IconButton id={`remove-recipe-name-${input.id}`}  color="primary" onClick={() => removeIngredient(input.id)} aria-label="remove ingredient" component="label">
+            <IconButton id={`remove-recipe-name-${input.id}`}  color="primary" onClick={() => props.removeIngredient(input.id)} aria-label="remove ingredient" component="label">
               <CloseIcon sx={{fontSize: '1.75rem'}}/>
             </IconButton>
           </div>
         )
       })}
 
-      <Button variant="contained" color="primary" className={classes['add-ingredient-btn']} onClick={addNewInput}>Add Ingredient</Button>
+      <Button variant="contained" color="primary" sx={{mb: '1rem'}} onClick={props.addIngredient}>Add Ingredient</Button>
     </React.Fragment>
   )
 }
